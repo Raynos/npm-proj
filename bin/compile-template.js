525de01073c3a43b172b3857c42dee8910bc7998
Raynos <raynos2@gmail.com>
2013-11-08 22:06:44 -0800

    compile template folder into js file

---
+++ bin/compile-template.js
@@ -0,0 +1,45 @@
+var fs = require("fs")
+var path = require("path")
+var parallel = require("continuable-para")
+
+var allFiles = require("../lib/all-files")
+
+var templateDir = path.join(__dirname, "..", "default-template")
+
+allFiles(templateDir, function (err, files) {
+    if (err) {
+        throw err
+    }
+
+    parallel(files.map(function (file) {
+        return function (callback) {
+            fs.readFile(file, function (err, source) {
+                if (err) {
+                    return callback(err)
+                }
+
+                callback(null, {
+                    file: file,
+                    relative: path.relative(templateDir, file),
+                    source: String(source)
+                })
+            })
+        }
+    }), function (err, tuples) {
+        if (err) {
+            throw err
+        }
+
+        var hash = tuples.reduce(function (acc, tuple) {
+            var key = tuple.relative
+            if (key === "._gitignore") {
+                key = ".gitignore"
+            }
+            acc[key] = tuple.source.split("\n")
+            return acc
+        }, {})
+
+        process.stdout.write("module.exports = " +
+            JSON.stringify(hash, null, "   ") + "\n")
+    })
+})
